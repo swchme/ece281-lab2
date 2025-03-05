ae33a4477ccad22be8ec28e11b3c804991599151
swchme <164097304+swchme@users.noreply.github.com>
2025-03-05 11:46:58 -0700

    m

--- src/sevenseg_decoder_tb.vhd
+++ src/sevenseg_decoder_tb.vhd
@@ -4,12 +4,12 @@ use IEEE.STD_LOGIC_1164.ALL;
  
 -- Uncomment the following library declaration if using
 -- arithmetic functions with Signed or Unsigned values
-use IEEE.NUMERIC_STD.ALL;
+--use IEEE.NUMERIC_STD.ALL;
  
 -- Uncomment the following library declaration if instantiating
 -- any Xilinx leaf cells in this code.
-library UNISIM;
-use UNISIM.VComponents.all;
+--library UNISIM;
+--use UNISIM.VComponents.all;
  
 entity sevenseg_decoder_tb is
 --    Port ( i_Hex : in STD_LOGIC_VECTOR (3 downto 0);
