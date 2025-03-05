43d56000e24985ed57a3271a5402bb44a293704a
swchme <164097304+swchme@users.noreply.github.com>
2025-03-04 23:47:58 -0700

    hi

--- src/sevenseg_decoder.vhd
+++ src/sevenseg_decoder.vhd
@@ -1,6 +1,6 @@
 ----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
+-- Company: USAFA 
+-- Engineer: Sweta Chandra Mohan 
 -- 
 -- Create Date: 02/25/2025 03:50:13 PM
 -- Design Name: 
@@ -32,13 +32,36 @@ use IEEE.STD_LOGIC_1164.ALL;
 --use UNISIM.VComponents.all;
 
 entity sevenseg_decoder is
-    Port ( i_Hex : in STD_LOGIC_VECTOR (3 downto 0);
-           o_seg_n : out STD_LOGIC_VECTOR (6 downto 0));
+    Port( 
+        i_Hex : in STD_LOGIC_VECTOR (3 downto 0);
+        o_seg_n : out STD_LOGIC_VECTOR (6 downto 0)
+    );
 end sevenseg_decoder;
 
 architecture Behavioral of sevenseg_decoder is
-
+    signal w_seg : STD_LOGIC_VECTOR (6 downto 0);
 begin
+    with i_Hex select 
+        w_seg <=   "1000000" when x"0",  -- 0
+                   "1111001" when x"1",  -- 1
+                   "0100100" when x"2",  -- 2
+                   "0110000" when x"3",  -- 3
+                   "0011001" when x"4",  -- 4
+                   "0010010" when x"5",  -- 5
+                   "0000010" when x"6",  -- 6
+                   "1111000" when x"7",  -- 7
+                   "0000000" when x"8",  -- 8
+                   "0010000" when x"9",  -- 9
+                   "0001000" when x"A",  -- A
+                   "0000011" when x"B",  -- B
+                   "1000110" when x"C",  -- C
+                   "0100001" when x"D",  -- D
+                   "0000110" when x"E",  -- E
+                   "0001110" when x"F",  -- F
+                   "1111111" when others; -- All segments off for undefined values
 
+    -- Active-low output
+    o_seg_n <= NOT w_seg; 
 
 end Behavioral;
+ 
--- src/top_basys3.vhd
+++ src/top_basys3.vhd
@@ -57,45 +57,62 @@
 library ieee;
   use ieee.std_logic_1164.all;
   use ieee.numeric_std.all;
-
-
+ 
+ 
 entity top_basys3 is
 	port(
 		-- 7-segment display segments (cathodes CG ... CA)
 		seg		:	out std_logic_vector(6 downto 0);  -- seg(6) = CG, seg(0) = CA
-
+ 
 		-- 7-segment display active-low enables (anodes)
 		an      :	out std_logic_vector(3 downto 0);
-
+ 
 		-- Switches
 		sw		:	in  std_logic_vector(3 downto 0);
-		
 		-- Buttons
 		btnC	:	in	std_logic
-
+ 
 	);
 end top_basys3;
-
+ 
 architecture top_basys3_arch of top_basys3 is 
-	
   -- declare the component of your top-level design
+   component sevenseg_decoder is
+   
+   
+   port ( i_Hex : in std_logic_vector (3 downto 0);
+   
+   
+          o_seg_n : out std_logic_vector (6 downto 0));
 
-
+   end component sevenseg_decoder;
+ 
   -- create wire to connect button to 7SD enable (active-low)
-
-  
+    signal w_7SD_EN_n : std_logic;
 begin
 	-- PORT MAPS ----------------------------------------
-
+    w_7SD_EN_n <= not btnC;
+    
+    
+    
+   an(0)    <= w_7SD_EN_n;
+   an(1)    <= '1';
+   an(2)    <= '1';
+   an(3)    <= '1';
+   
+   
 	--	Port map: wire your component up to the switches and seven-segment display cathodes
 	-----------------------------------------------------	
+	sevenseg_decoder_0 : sevenseg_decoder
+	port map (
 	
-	
+	   i_Hex => sw(3 downto 0),
+	   o_seg_n => seg(6 downto 0));
+	   
+	   
 	-- CONCURRENT STATEMENTS ----------------------------
-	
 	-- wire up active-low 7SD anode (active low) to button (active-high)
 	-- display 7SD 0 only when button pushed
 	-- other 7SD are kept off
 	-----------------------------------------------------
-	
 end top_basys3_arch;
